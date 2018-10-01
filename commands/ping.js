868c0f2e6ffcaf9f68598cb9f8eb9b4a9c3169c9
realXAII <mini.shadz0479@gmail.com>
2018-10-01 23:28:08 +1000

    stuff

--- commands/ping.js
+++ commands/ping.js
@@ -4,19 +4,35 @@ const client = new Discord.Client();
 
 
 module.exports.run = async (bot, message, args) => {
+ 
     
-let botpinggg = new Discord.RichEmbed()
+    
+    let botpinggg = new Discord.RichEmbed()
+    .setTitle(`Pong! The current MS of the bot is ${bot.ping}!`)
+    .setColor('00ff00')
+    .setTimestamp();
+    
+    
+    
+    
+    
 
-.setTitle(`Pong! The current MS of the bot is ${bot.ping}!`)
-.setColor('00ff00')
-.setTimestamp();
+        
+    
+    message.channel.send(botpinggg);
+    
+    
+  
+    
+    
+    
+    }
+    
 
 
 
 
 
-message.channel.send(botpinggg)
 
 
 
-}
