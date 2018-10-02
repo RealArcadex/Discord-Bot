00076574866c678fa8b4ed4bcf383e80fe5d1137
realXAII <mini.shadz0479@gmail.com>
2018-10-02 20:05:49 +1000

    revamped -dab

--- commands/dab.js
+++ commands/dab.js
@@ -3,13 +3,40 @@ const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
 
+    let outcomes = [
+        "#ff0000",
+        "#ff7f00",
+        "#fff000",
+        "#15ff00",
+        "#00ff77",
+        "#00ff77.",
+        "#0400ff",
+        "#ae00ff",
+        "#ff00dc",
+        "#ff0054"
+    
+     ]
+ 
+ 
+     
+     let _outcomes = Math.floor(Math.random()*outcomes.length);
+     let outcome = outcomes[_outcomes]
+
+
+
+
+
+
+
+
+
 let dabbb = new Discord.RichEmbed()
 .setTitle('<o/')
-.setColor('#8d00d8');
+.setColor(`${outcome}`)
 
 
     
-message.channel.send(dabbb)
+message.channel.send(dabbb);
 
 
 
