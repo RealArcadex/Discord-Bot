82af3a36c68a497660343e930621c034d2d5a785
realXAII <mini.shadz0479@gmail.com>
2018-11-04 20:20:14 +1100

    a=changed statyid, boicechannel, zepxloasuve coammdsn

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,8 @@ module.exports = client => {
     console.log(`Connected to Discord as ${client.user.tag}.\n--= Stats =--\n\nServers: ${client.guilds.size}`);
     client.user.setPresence({ 
         game: { 
-            name: 'Jynx | ' + client.guilds.size + ' Severs'
+            name: 'Jynx | -help'
         },
-        status: 'online'
+        status: 'watching'
     }).catch(console.error);
 }
