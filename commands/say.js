4d3b6d20230e3bb980a8ea52237419eff626d8a1
realxaii <mini.shadz0479@gmail.com>
2018-12-15 10:55:39 +1100

    chistmas and ownersay command - fixed say.js

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
 
+
+    
     let sayMessage = new Discord.RichEmbed()
     .setTitle(args.join(" "))
     .setColor('#4e42f4')
